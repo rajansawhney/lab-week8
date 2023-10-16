e260e8fda628e475d80df368c1c07439a4b74927
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-10-16 13:40:32 -1000

    Initial commit

---
+++ average.c
@@ -0,0 +1,8 @@
+/*
+Write a C Program that finds and prints the average of n numbers provided by the user.
+
+NOTE:
+1. Accept only integers
+2. Display output as:
+    Average = <average of n numbers>
+*/
